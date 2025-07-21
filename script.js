9fae24a43bb224a98b8b971451099c4956cf24dc
VINISHASUDEVAN <sudevanvinisha@gmail.com>
2025-07-21 15:44:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log()
+console.log()
+function subtract()
+{
+   var num1 =(document.getElementById("number1").value)
+   var num2 = (document.getElementById("number2").value)
+   console.log(num1-num2)
+}
+function add()
+{
+    var num1 = parseFloat(document.getElementById("number1").value)
+    var num2 = parseFloat(document.getElementById("number2").value)
+    console.log(num1+num2)
+}
+function multiply()
+{
+    var num1 = (document.getElementById("number1").value)
+    var num2 = (document.getElementById("number2").value)
+    console.log(num1*num2)
+}
+
+

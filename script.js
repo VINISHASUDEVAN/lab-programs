dae7855ae7482b2c3680f93116add9ba6c749a45
VINISHASUDEVAN <sudevanvinisha@gmail.com>
2025-07-23 15:46:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,5 +18,13 @@ function multiply()
     var num2 = (document.getElementById("number2").value)
     console.log(num1*num2)
 }
+function divide() 
+{
+    var num1 = parseFloat(document.getElementById("number1").value);
+    var num2 = parseFloat(document.getElementById("number2").value);
+    console.log(num1/num2)
+}
+
+    
 
 

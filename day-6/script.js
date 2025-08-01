b4b421544b7d2d16a646ed2f27a0befc9403d669
VINISHASUDEVAN <sudevanvinisha@gmail.com>
2025-08-01 15:26:59 +0530

    Create script.js

---
+++ day-6/script.js
@@ -0,0 +1,27 @@
+class People
+{
+    constructor(firstName,mark)
+    {
+        this.firstName  = firstName 
+        this.mark = mark
+    }
+}                       
+const obj2=new People("vinisha",100)   
+const obj3=new People("rethika",100) 
+console.log(obj2.firstName) 
+//declaration of an array                                                                                                                                                              }
+const arr=[5,4,3,2,1,6,7,8,9]
+//for each
+arr.forEach((x)=>{console.log(x)})
+//filter-get it into it
+ let filteredArr=arr.filter((x)=>x%2==0)
+ console.log( filteredArr)
+ //reduce
+ let sum = arr.reduce((acc,val)=>acc+val)
+ console.log(sum)
+ //slice
+ let copyArr= arr.slice(2)
+ console.log(copyArr)
+ //splice
+ arr.splice(2,4)
+ console.log(arr)
